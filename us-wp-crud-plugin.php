36ced22ef3fa7033b32fab1ddad7b63a76f11bbd
Md. Russel Hussain <md.russel.hussain@gmail.com>
2021-10-01 23:06:08 +0600

    header updated

---
+++ us-wp-crud-plugin.php
@@ -0,0 +1,24 @@
+/**
+* Plugin Name
+*
+* @package BasicCRUD
+* @author Md. Russel Hussain
+* @copyright 2021 rus.org
+* @license GPL-2.0-or-later
+*
+* @wordpress-plugin
+* Plugin Name: Basic CRUD
+* Plugin URI: https://russel.authlab/plugins
+* Description: Custom CRUD features for admin
+* Version: 1.0.0
+* Requires at least: 5.2
+* Requires PHP: 7.2
+* Author: Md. Russel Hussain
+* Author URI: https://russel.authlab
+* Author URI: https://author.example.com/
+* License: GPL v2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Update URI: https://example.com/my-plugin/
+* Text Domain: my-basics-plugin
+* Domain Path: /languages
+*/
